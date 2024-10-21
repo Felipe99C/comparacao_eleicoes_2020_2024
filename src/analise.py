61b0977011bdf6454ddf74726f511373615d35a5
Felipe <felipe.r.cardoso@outlook.com>
2024-10-21 03:06:59 -0300

    analise comparativa do grafico de 2012 a 2024

--- src/analise.py
+++ src/analise.py
@@ -44,16 +44,16 @@ with ZipFile(arquivo_resultado_2012) as z:
 
 #%%
 # mostra todas as colunas e tipos de dados
-resultado_2024.info()
+resultado_2016.info()
 
 #%%
-## Pega os dados dos partidos
+## Pega os dados dos partidos dentro do espectro politico
 
 partidos = pd.read_csv(arquivo_partidos_2024, sep=';')
 
 espec_type = pd.CategoricalDtype(categories=['direita', 'centro', 'esquerda'], ordered=True)
 partidos['Espectro'] = partidos['Espectro'].astype(espec_type)
-
+#%%
 partidos.info()
 
 #%%
@@ -67,24 +67,120 @@ geo_df['NM_MUN'] = geo_df['NM_MUN'].str.upper()
 geo_df
 
 #%%
+###################################### Analise dos espectros vais votados no primeiro turno nas eleições #####################################################
 
- 
-votos_espectro_2024 = resultado_2024[(resultado_2024['DS_CARGO'] == TIPO_CANDIDATO)&(resultado_2024['NR_TURNO']==1)].groupby(['CD_MUNICIPIO', 'Espectro']).agg({'NM_URNA_CANDIDATO':'first'
-                                                                                                                                                               ,'NM_MUNICIPIO':'first'
-                                                                                                                                                               ,'SG_UF':'first'
-                                                                                                                                                               ,'QT_VOTOS_NOMINAIS_VALIDOS':'sum'}).reset_index()
+# Faz o merge dos partidos com o espectro politico
+
+resultado_2024 = resultado_2024.\
+merge(partidos[['NR_PARTIDO','Espectro']], left_on='NR_PARTIDO', right_on='NR_PARTIDO', suffixes=('', '_partido'))
+
+votos_espectro_2024 = resultado_2024[(resultado_2024['DS_CARGO']== TIPO_CANDIDATO)&(resultado_2024['NR_TURNO']==1)].groupby(['CD_MUNICIPIO','Espectro']).agg({'NM_MUNICIPIO':'first','SG_UF':'first','QT_VOTOS_NOMINAIS_VALIDOS':'sum'}).reset_index()
 espectro_mais_votado_2024 = votos_espectro_2024.loc[votos_espectro_2024.groupby('CD_MUNICIPIO')['QT_VOTOS_NOMINAIS_VALIDOS'].idxmax()].reset_index(drop=True)
 espectro_mais_votado_2024
 
+resultado_2020 = resultado_2020.\
+merge(partidos[['NR_PARTIDO','Espectro']], left_on='NR_PARTIDO', right_on='NR_PARTIDO',suffixes=('', '_partido'))
+
+votos_espectro_2020 = resultado_2020[(resultado_2020['DS_CARGO']== TIPO_CANDIDATO)&(resultado_2020['NR_TURNO']==1)].groupby(['CD_MUNICIPIO','Espectro']).agg({'NM_MUNICIPIO':'first','SG_UF':'first','QT_VOTOS_NOMINAIS_VALIDOS':'sum'}).reset_index()
+espectro_mais_votado_2020 = votos_espectro_2020.loc[votos_espectro_2020.groupby('CD_MUNICIPIO')['QT_VOTOS_NOMINAIS_VALIDOS'].idxmax()].reset_index(drop=True)
+espectro_mais_votado_2020
+
+resultado_2016 = resultado_2016.\
+merge(partidos[['NR_PARTIDO','Espectro']], left_on='NR_PARTIDO', right_on='NR_PARTIDO',suffixes=('', '_partido'))
+
+resultado_2012 = resultado_2012.\
+merge(partidos[['NR_PARTIDO','Espectro']], left_on='NR_PARTIDO', right_on='NR_PARTIDO',suffixes=('', '_partido'))
 
 
 #%%
 
-resultado_2024 = resultado_2024.\
-merge(partidos[['NR_PARTIDO','Espectro']], left_on='NR_PARTIDO', right_on='NR_PARTIDO')
+# Grafico comparando os espectros mais votados em 2020 e 2024
+espectro_mais_votado_2024_geo_df = geo_df.merge(espectro_mais_votado_2024, left_on='NM_MUN', right_on='NM_MUNICIPIO')
+espectro_mais_votado_2020_geo_df = geo_df.merge(espectro_mais_votado_2020, left_on='NM_MUN', right_on='NM_MUNICIPIO')
 
-resultado_2020 = resultado_2020.\
-merge(partidos[['NR_PARTIDO','Espectro']], left_on='NR_PARTIDO', right_on='NR_PARTIDO')
+fig, ax = plt.subplots(1,2,figsize=(28,10))
+ax[0].set_title(f'Espectro Mais Votado {LOCAL} 2020 ({TIPO_CANDIDATO})', color='black', size=16)
+ax[1].set_title(f'Espectro Mais Votado {LOCAL} 2024 ({TIPO_CANDIDATO})', color='black', size=16)
 
-resultado_2016 = resultado_2016.\
-merge(partidos[['NR_PARTIDO', 'Espectro']], left_on='NR_PARTIDO', right_on='NR_PARTIDO')
+espectro_mais_votado_2020_geo_df.plot(column='Espectro', legend=True, cmap='coolwarm', ax=ax[0])
+espectro_mais_votado_2024_geo_df.plot(column='Espectro', legend=True, cmap='coolwarm', ax=ax[1])
+
+
+#%%
+
+############################ Analise do espectro dos candidatos mais votados ######################################################
+candidatos_mais_votados_2024 = resultado_2024[(resultado_2024['DS_CARGO']== TIPO_CANDIDATO)&(resultado_2024['NR_TURNO']== 1)]\
+.groupby(['NR_CANDIDATO','NM_UE'])\
+.agg({'Espectro' :'first','NM_URNA_CANDIDATO' :'first', 'NM_MUNICIPIO' :'first', 'CD_MUNICIPIO' :'first',
+      'DS_SIT_TOT_TURNO' :'first', 'NR_PARTIDO' :'first',
+      'SG_PARTIDO' :'first','QT_VOTOS_NOMINAIS' : 'sum' })\
+      .sort_values(by='QT_VOTOS_NOMINAIS',ascending=False)\
+      .drop_duplicates(subset=['CD_MUNICIPIO'],keep='first')
+
+
+candidatos_mais_votados_2020 = resultado_2020[(resultado_2020['DS_CARGO']== TIPO_CANDIDATO) & (resultado_2020['NR_TURNO']== 1)]\
+.groupby(['NR_CANDIDATO','NM_UE'])\
+.agg({'Espectro' :'first','NM_URNA_CANDIDATO' :'first', 'NM_MUNICIPIO' :'first', 'CD_MUNICIPIO' :'first',
+      'DS_SIT_TOT_TURNO' :'first', 'NR_PARTIDO' :'first',
+      'SG_PARTIDO' :'first','QT_VOTOS_NOMINAIS' : 'sum' })\
+      .sort_values(by='QT_VOTOS_NOMINAIS',ascending=False)\
+      .drop_duplicates(subset=['CD_MUNICIPIO'],keep='first')
+
+candidatos_mais_votados_2016 = resultado_2016[(resultado_2016['DS_CARGO']==TIPO_CANDIDATO)&(resultado_2016['NR_TURNO']== 1)]\
+.groupby(['NR_CANDIDATO','NM_UE'])\
+.agg({'Espectro' :'first','NM_URNA_CANDIDATO' :'first', 'NM_MUNICIPIO' :'first', 'CD_MUNICIPIO' :'first',
+      'DS_SIT_TOT_TURNO' :'first', 'NR_PARTIDO' :'first',
+      'SG_PARTIDO' :'first','QT_VOTOS_NOMINAIS' : 'sum' })\
+      .sort_values(by='QT_VOTOS_NOMINAIS',ascending=False)\
+      .drop_duplicates(subset=['CD_MUNICIPIO'],keep='first')
+
+candidatos_mais_votados_2012 = resultado_2012[(resultado_2012['DS_CARGO']==TIPO_CANDIDATO)&(resultado_2012['NR_TURNO']== 1)]\
+.groupby(['NR_CANDIDATO','NM_UE'])\
+.agg({'Espectro' :'first','NM_URNA_CANDIDATO' :'first', 'NM_MUNICIPIO' :'first', 'CD_MUNICIPIO' :'first',
+      'DS_SIT_TOT_TURNO' :'first', 'NR_PARTIDO' :'first',
+      'SG_PARTIDO' :'first','QT_VOTOS_NOMINAIS' : 'sum' })\
+      .sort_values(by='QT_VOTOS_NOMINAIS',ascending=False)\
+      .drop_duplicates(subset=['CD_MUNICIPIO'],keep='first')
+#%%
+
+# Analisa a qtd de candidatos votados por partidos de 2012 a 2024 
+
+#candidatos_mais_votados_2024.SG_PARTIDO.value_counts().plot(kind='bar',title=f'Partido Mais Votados {LOCAL} 2024 ({TIPO_CANDIDATO})')
+
+#candidatos_mais_votados_2020.SG_PARTIDO.value_counts().plot(kind='bar',title=f'Partido Mais Votado {LOCAL} 2020 ({TIPO_CANDIDATO})')
+
+#candidatos_mais_votados_2016.SG_PARTIDO.value_counts().plot(kind='bar', title=f'Partido Mais Votado {LOCAL} 2016 ({TIPO_CANDIDATO})')
+
+candidatos_mais_votados_2012.SG_PARTIDO.value_counts().plot(kind='bar', title=f'Partido Mais Votado {LOCAL} 2012 ({TIPO_CANDIDATO})')
+
+#%%
+candidatos_mais_votados_2024_geo_df = geo_df.merge(candidatos_mais_votados_2024, left_on='NM_MUN', right_on='NM_MUNICIPIO')
+candidatos_mais_votados_2020_geo_df = geo_df.merge(candidatos_mais_votados_2020, left_on='NM_MUN', right_on='NM_MUNICIPIO')
+candidatos_mais_votados_2016_geo_df = geo_df.merge(candidatos_mais_votados_2016, left_on='NM_MUN', right_on='NM_MUNICIPIO')
+candidatos_mais_votados_2012_geo_df = geo_df.merge(candidatos_mais_votados_2012, left_on='NM_MUN', right_on='NM_MUNICIPIO')
+
+fig, ax = plt.subplots(1,2,figsize=(28,10))
+ax[0].set_title(f'Espectro Candidato Mais Votado {LOCAL} 2020 ({TIPO_CANDIDATO})', color='black', size=16)
+ax[1].set_title(f'Espectro Candidato Mais Votado {LOCAL} 2024 ({TIPO_CANDIDATO})', color='black', size=16)
+
+candidatos_mais_votados_2020_geo_df.plot(column='Espectro', legend=True, cmap='coolwarm', ax=ax[0])
+candidatos_mais_votados_2024_geo_df.plot(column='Espectro', legend=True, cmap='coolwarm', ax=ax[1])
+#%%
+
+fig, ax = plt.subplots(1,4,figsize=(28,10))
+
+# Definindo títulos para os subplots
+ax[0].set_title(f'Espectro Mais Votados {LOCAL} 2012 ({TIPO_CANDIDATO})', color='black', size=16)
+ax[1].set_title(f'Espectro Mais Votados {LOCAL} 2016 ({TIPO_CANDIDATO})', color='black', size=16)
+ax[2].set_title(f'Espectro Mais Votados {LOCAL} 2020 ({TIPO_CANDIDATO})', color='black', size=16)
+ax[3].set_title(f'Espectro Mais Votados {LOCAL} 2024 ({TIPO_CANDIDATO})', color='black', size=16)
+
+# Plotando os dados nos subplots corretos
+candidatos_mais_votados_2012_geo_df.plot(column='Espectro', legend=True, cmap='coolwarm', ax=ax[0])
+candidatos_mais_votados_2016_geo_df.plot(column='Espectro', legend=True, cmap='coolwarm', ax=ax[1])
+candidatos_mais_votados_2020_geo_df.plot(column='Espectro', legend=True, cmap='coolwarm', ax=ax[2])
+candidatos_mais_votados_2024_geo_df.plot(column='Espectro', legend=True, cmap='coolwarm', ax=ax[3])
+
+# Ajustando o layout para evitar sobreposição
+plt.tight_layout()
+plt.show()
